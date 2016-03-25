86d8b7ecee4b49cd21f237863038db954f6a9855
ricardo ruas <ricardoruas88@gmail.com>
2016-03-25 00:33:27 -0300

    first commit

---
+++ GatewayPagamento/src/main/java/com/softwarecwb/GatewayPagamentoApplication.java
@@ -0,0 +1,12 @@
+package com.softwarecwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayPagamentoApplication.class, args);
+	}
+}
